9ce55cade762cdcda2802ac7fa7b32052da36b4b
Vladislav Rusakov <htpw@ya.ru>
2022-07-18 00:33:12 +0300

    fullscreen navigation

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const fullScreenMenu = document.querySelector(".fullscreen-menu");
 const close = document.querySelector(".fullscreen-menu__close");
+const closeForTouch = $(fullScreenMenu).find(".menu__link");
+
+console.log(closeForTouch);
 
 hamburger.addEventListener("click", e => {
   e.preventDefault();
@@ -9,12 +12,16 @@ hamburger.addEventListener("click", e => {
   if (actualDisplay == "none") {
     fullScreenMenu.style.display = "flex";
   }
-})
+});
 
 close.addEventListener("click", e2 => {
   e2.preventDefault();
 
   fullScreenMenu.style.display = "none";
-})
+});
 
+$(closeForTouch).click( e3 => {
+  e3.preventDefault();
 
+  fullScreenMenu.style.display = "none";
+});
